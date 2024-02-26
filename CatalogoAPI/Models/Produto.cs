70409cca57ee6d09655fe4eb629aebb95d6d97a3
riverson98 <riversonvicente@gmail.com>
2024-02-26 09:03:56 -0300

    Cria validação personalizada para atributos

--- CatalogoAPI/Models/Produto.cs
+++ CatalogoAPI/Models/Produto.cs
@@ -1,23 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CatalogoAPI.Validations;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace CatalogoAPI.Models;
 
-public class Produto
+public class Produto : IValidatableObject
 {
+    [JsonIgnore]
     public int ProdutoId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "O nome é obrigatorio")]
     [StringLength(80)]
+    //[PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "A Descrição é obrigatoria")]
     [StringLength(300)]
     public string? Descricao { get; set; }
 
     [Required]
     [Column(TypeName ="decimal(10,2)")]
+    [Range(1, 50000)]
     public decimal Preco { get; set; }
 
     [Required]
@@ -29,4 +34,23 @@ public class Produto
     
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome.ToString()[0]
+                .ToString();
+
+            if (!primeiraLetra.Equals(primeiraLetra.ToUpper()))
+                yield return 
+                    new ValidationResult("A primeira letra do nome do produto deve ser maiúscula",
+                    new[] {nameof(this.Nome)});
+        }
+
+        if (this.Estoque <= 0)
+            yield return new ValidationResult("O estoque dedve ser maior que zero", new[] {
+                nameof(this.Estoque) 
+            });
+    }       
 }

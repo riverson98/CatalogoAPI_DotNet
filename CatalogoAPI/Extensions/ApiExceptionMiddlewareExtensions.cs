b990606f1dc4f438e33690d9db8c83d9d598a708
riverson98 <riversonvicente@gmail.com>
2024-02-26 10:06:03 -0300

    Move pasta extensions de lugar

---
+++ CatalogoAPI/Extensions/ApiExceptionMiddlewareExtensions.cs
@@ -0,0 +1,33 @@
+﻿using CatalogoAPI.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace CatalogoAPI.Extensions
+{
+    public static class ApiExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (contextFeature is not null)
+                    {
+                        await context.Response.WriteAsync(new DetalhesDeErro()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Mensagem = contextFeature.Error.Message,
+                            Rastro = contextFeature.Error.StackTrace
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
